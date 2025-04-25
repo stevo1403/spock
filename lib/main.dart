b8df39786c5dd43f5307786e6f25dae11f3edec4
adebayosteven1403 <adebayosteven1403@gmail.com>
2025-04-25 17:35:54 +0000

    feat: replace main app with web3 main

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,119 @@
-// ignore_for_file: prefer_const_constructors
-import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:pure_wallet_2/pages/hompage.dart';
-import 'package:pure_wallet_2/static/init_client.dart';
-import 'static/scaled_size_custom.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-Future<void> main() async {
-  await initializeApp();
-  // Run the app with the initial locale
-  // SharedPreferences prefs = await SharedPreferences.getInstance();
-  runApp(MyApp());
+import 'providers/wallet_provider.dart';
+import 'providers/message_provider.dart';
+import 'providers/advertisement_provider.dart';
+import 'providers/token_provider.dart';
+import 'providers/theme_provider.dart';
+import 'screens/main_screen.dart';
+import 'utils/theme.dart';
+import 'services/notification_service.dart';
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  // Set preferred orientations
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+  
+  // Set system UI overlay style
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.light,
+      systemNavigationBarColor: AppTheme.backgroundColor,
+      systemNavigationBarIconBrightness: Brightness.light,
+    ),
+  );
+  
+  // Initialize notifications
+  await NotificationService.initialize(flutterLocalNotificationsPlugin);
+  
+  runApp(const MyApp());
 }
 
-Future<void> initializeApp() async {
-  // Set up the global client
-  web3Client.setClient();
-}
-
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  MyAppState createState() => MyAppState();
-}
-
-class MyAppState extends State<MyApp> with WidgetsBindingObserver {
-  @override
-  void initState() {
-    super.initState();
-    // _router = router;
-  }
-
-  @override
-  void dispose() {
-    WidgetsBinding.instance.removeObserver(this);
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    ScaledSizeCustom.setInitMediaQuerySize(context);
-    return MaterialApp(
-      home: HomePage()
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => ThemeProvider()),
+        ChangeNotifierProvider(create: (_) => WalletProvider()),
+        ChangeNotifierProvider(create: (_) => MessageProvider()),
+        ChangeNotifierProvider(create: (_) => AdvertisementProvider()),
+        ChangeNotifierProvider(create: (_) => TokenProvider()),
+      ],
+      child: Consumer<ThemeProvider>(
+        builder: (context, themeProvider, _) {
+          return MaterialApp(
+            title: 'Web3 Wallet Connect',
+            debugShowCheckedModeBanner: false,
+            theme: AppTheme.lightTheme,
+            darkTheme: AppTheme.darkTheme,
+            themeMode: themeProvider.isDarkMode ? ThemeMode.dark : ThemeMode.light,
+            home: const MainScreen(),
+          );
+        },
+      ),
     );
   }
 }
+
+// START_FILE
+// // ignore_for_file: prefer_const_constructors
+// import 'dart:async';
+// import 'package:flutter/material.dart';
+// import 'package:pure_wallet_2/pages/hompage.dart';
+// import 'package:pure_wallet_2/static/init_client.dart';
+// import 'static/scaled_size_custom.dart';
+
+// Future<void> main() async {
+//   await initializeApp();
+//   // Run the app with the initial locale
+//   // SharedPreferences prefs = await SharedPreferences.getInstance();
+//   runApp(MyApp());
+// }
+
+// Future<void> initializeApp() async {
+//   // Set up the global client
+//   web3Client.setClient();
+// }
+
+// class MyApp extends StatefulWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   MyAppState createState() => MyAppState();
+// }
+
+// class MyAppState extends State<MyApp> with WidgetsBindingObserver {
+//   @override
+//   void initState() {
+//     super.initState();
+//     // _router = router;
+//   }
+
+//   @override
+//   void dispose() {
+//     WidgetsBinding.instance.removeObserver(this);
+//     super.dispose();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     ScaledSizeCustom.setInitMediaQuerySize(context);
+//     return MaterialApp(
+//       home: HomePage()
+//     );
+//   }
+// }
